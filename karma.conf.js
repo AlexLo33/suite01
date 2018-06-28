0836bb85664134feaab51d64a514f7e3e71107f0
MickaelVoirin <36167919+MickaelVoirin@users.noreply.github.com>
2018-06-28 16:11:41 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,6 @@ module.exports = function(config) {
   }
 
   config.set({
-    browsers: ['Chrome', 'google-chrome-stable'],
     frameworks: ['jasmine', 'browserify'],
     preprocessors: {
       'tests/**/*.spec.js': ['browserify']
